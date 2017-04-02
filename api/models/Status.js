2cdb2c534459eb0ebece17a223abd7c56be1b21e
madhuhema <madhunarmi@gmail.com>
2017-04-02 10:46:24 +0530

    adding status

---
+++ api/models/Status.js
@@ -0,0 +1,39 @@
+/**
+ * Status.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+	approver_id: {
+			model:'user',
+			required:true
+		},
+	status: {
+			type: 'string',
+			required: true
+		},
+	approved_at: {
+			type: 'datetime',
+			required: true
+		},
+	alloted_time_interval: {
+			type: 'integer',
+			required: true
+		},
+	proposal_id:{
+		model:'proposal',
+		required:true
+	 },
+	last_update:{
+		type:'datetime',
+		required:'true'
+	}
+
+  }
+};
+
